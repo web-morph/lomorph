f9723fa222d988e7787b8cc0b43410b71fdf9acd
CKATEPTb <ckateptb@vivaldi.net>
2025-07-16 17:11:18 +0300

    change repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -63,11 +63,11 @@ publishing {
     }
     repositories {
         maven {
-            name = "jyrafRepo"
-            url = uri("https://repo.jyraf.com/repository/maven-releases/")
+            name = "BillmarsSoft"
+            url = uri("https://repo.billmarssoft.com/releases/")
             credentials {
-                username = System.getenv("NEXUS_USERNAME")
-                password = System.getenv("NEXUS_PASSWORD")
+                username = System.getenv("REPOSITORY_USERNAME")
+                password = System.getenv("REPOSITORY_PASSWORD")
             }
         }
     }
